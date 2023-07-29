be4e96b3153e2cd9a2ca649f9a536c23354cea53
aelabid <asmaa9920@hotmail.com>
2023-07-29 22:43:21 +0100

    Add husky configuration
    --format the code before pushing it

--- ft_transcendence/backend/srcs/.eslintrc.js
+++ ft_transcendence/backend/srcs/.eslintrc.js
@@ -15,21 +15,21 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
   rules: {
-    //the interface name should start with I
-    '@typescript-eslint/interface-name-prefix': 'off',
     // you are not required to explicitly declare return types for your functions
     '@typescript-eslint/explicit-function-return-type': 'off',
     //Allows omitting explicit return types on functions and methods within classes
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     // Flags unused variables to ensure code cleanliness.
+    "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error",
     // Disallows using the any type to encourage explicit typing.
     '@typescript-eslint/no-explicit-any': 'error',
     //Enforces naming conventions specific to NestJS, such as using PascalCase for class names and camelCase for variables and methods.
     //see "Naming conventions specific to NestJS" in "NOTES" list.
-    '@nestjs-eslint/naming-convention': ['error',
+    //the interface name shouldn't start with I
+    '@typescript-eslint/naming-convention': ['error',
       {
         selector: 'default',
         format: ['camelCase'],
@@ -39,21 +39,20 @@ module.exports = {
         format: ['camelCase', 'UPPER_CASE'],
       },
       {
-        selector: 'typeLike',
+        selector: ['typeLike', 'interface'],
         format: ['PascalCase'],
       },
     ],
     //Discourages the use of magic numbers without explicit explanation or constants.
-    "@nestjs-eslint/no-magic-numbers": "error",
-    //Detects duplicate imports of modules within the same file.
-    "@nestjs-eslint/no-duplicate-modules": "error",
+    "no-magic-numbers": "off",
+    "@typescript-eslint/no-magic-numbers": "error",
     //Detects unnecessary classes that don't provide any functionality.
-    "@nestjs-eslint/no-extraneous-class": "error",
+    "@typescript-eslint/no-extraneous-class": "error",
 
 
     // Code style and formatting
     indent: ['error', 2],
-    quotes: ['error', 'single'],
+    // quotes: ['error', 'single'],
     semi: ['error', 'always'],
   },
 };
