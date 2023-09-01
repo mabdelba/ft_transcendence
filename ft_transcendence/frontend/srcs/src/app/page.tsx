726f6c5aa553b6b0674d38bc7192cc7888936e62
ahmed el bahri <bahri.tanger10@gmail.com>
2023-09-01 20:02:56 +0100

    fixed npm run lint errors

--- ft_transcendence/frontend/srcs/src/app/page.tsx
+++ ft_transcendence/frontend/srcs/src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image'
-
-export default function Home() {
+export default function home() {
   return (
     <main className="h-screen">
       <div className="ft_background">
