2e276bfd4711a29951d067804911a852362a2d42
mabdelba <abdelbar.mohamed1998@gmail.com>
2023-09-12 19:11:46 +0100

    add error message, handle blur

--- ft_transcendence/frontend/srcs/app/register/page.tsx
+++ ft_transcendence/frontend/srcs/app/register/page.tsx
@@ -13,135 +13,156 @@ import blackQuarante from '../../public/black42.svg';
 import { useState } from 'react';
 
 function Register() {
-  var Regex = /\b([a-zA-ZÀ-ÿ][-a-zA-ZÀ-ÿ. ']+[ ]*)+/;
-  var EmRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  var PassRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+	var Regex = /\b([a-zA-ZÀ-ÿ][-a-zA-ZÀ-ÿ. ']+[ ]*)+/;
+	var EmRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+	var PassRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+	var UserRegex = /^[^\s]+$/;
 
-  const [firstname, setFirstName] = useState('');
-  const [lastname, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [vpassword, setVerif] = useState('');
-  const [ferror, setFerror] = useState(true);
-  const [lerror, setLerror] = useState(true);
-  const [eerror, setEerror] = useState(true);
-  const [uerror, setUerror] = useState(true);
-  const [perror, setPerror] = useState(true);
-  const [verror, setVerror] = useState(true);
-  // const [valid, setValid] = useState(false);
+	const [firstname, setFirstName] = useState('');
+	const [lastname, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [vpassword, setVerif] = useState('');
+	const [ferror, setFerror] = useState(false);
+	const [lerror, setLerror] = useState(false);
+	const [eerror, setEerror] = useState(false);
+	const [uerror, setUerror] = useState(false);
+	const [perror, setPerror] = useState(false);
+	const [verror, setVerror] = useState(false);
+	const [valid, setValid] = useState(false);
 
-  const Data = { firstname, lastname, username, email, password };
+	const Data = { firstname, lastname, username, email, password, vpassword };
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    if (Data.firstname == '') return setFerror(false);
-    if (Data.lastname == '') return setLerror(false);
-    if (Data.username == '') return setUerror(false);
-    if (Data.email == '') return setEerror(false);
-    if (Data.password == '') return setPerror(false);
-    if (!ferror || !lerror || !uerror || !eerror || !perror || !verror) return;
-    console.log(Data);
-  };
+	const handleSubmit = (event: any) => {
+		event.preventDefault();
+		// if (Data.firstname == '') setValid(false);
+		// if (Data.lastname == '')  setValid(false);
+		// if (Data.username == '')  setValid(false);
+		// if (Data.email == '')  setValid(false);
+		// if (Data.password == '')  setValid(false);
+		if (!ferror || !lerror || !uerror || !eerror || !perror || !verror) 
+			return (alert("Please fill out all fields with compatible format!"));
+		// if(valid)
+		console.log(Data);
+		// else
+		//   alert("Please fill out all fields with supported formats!");
+	 
+	};
 
-  const handleBlur = () => {
-    const regFirstName = Regex.test(Data.firstname);
-    if (!regFirstName && Data.firstname != '') setFerror(false);
-    else setFerror(true);
-    if (Data.username != '') setUerror(true);
-    const regLastName = Regex.test(Data.lastname);
-    if (!regLastName && Data.lastname != '') setLerror(false);
-    else setLerror(true);
-    const regEmail = EmRegex.test(Data.email);
-    if (!regEmail && Data.email != '') setEerror(false);
-    else setEerror(true);
-    if (!PassRegex.test(Data.password) && Data.password != '') setPerror(false);
-    else setPerror(true);
-    if (vpassword !== password) setVerror(false);
-    else setVerror(true);
-  };
 
-  return (
-    <main className="flex justify-center items-center bg-[#282828] w-screen h-screen">
-      <div className="px-2 py-1 flex flex-col justify-center min-w-[280px] min-h-[479px] w-full h-[90%] md:w-2/3  lg:w-1/3  lg:h-[80%] bg-black NeonShadowBord">
-        <div className="h-1/6 w-full  flex flex-col">
-          <div className="flex justify-end items-center h-1/5 w-full ">
-            <Link href="/">
-              <Image src={close} alt="close" className="w-10 h-10" />
-            </Link>
-          </div>
-          <div className="w-full h-4/6 text-white text-3xl font-bold NeonShadow font-Orbitron flex justify-center items-center">
-            <h1>Atari Pong</h1>
-          </div>
-        </div>
-        <form onSubmit={handleSubmit} className=" w-full h-5/6 flex  flex-col space-y-3 px-2">
-          <div className="h-[10%] w-full flex flex-row justify-center space-x-2">
-            <div onBlur={handleBlur} className="w-1/2 h-full">
-              <SimpleInput
-                holder="First Name"
-                type1="text"
-                SetValue={setFirstName}
-                error={ferror}
-              />
-            </div>
-            <div onBlur={handleBlur} className="w-[50%] h-full">
-              <SimpleInput holder="Last Name" type1="text" SetValue={setLastName} error={lerror} />
-            </div>
-          </div>
-          <div onBlur={handleBlur} className="h-[10%] w-full ">
-            <SimpleInput holder="Username" type1="text" SetValue={setUsername} error={uerror} />
-          </div>
-          <div onBlur={handleBlur} className="h-[10%] w-full ">
-            <SimpleInput
-              holder="Mail@example.com"
-              type1="email"
-              SetValue={setEmail}
-              error={eerror}
-            />
-          </div>
-          <div onBlur={handleBlur} className="h-[10%] w-full ">
-            <SimpleInput
-              holder="Password"
-              type1="password"
-              type2="text"
-              icon={seePassword}
-              error={perror}
-              SetValue={setPassword}
-            />
-          </div>
-          <div onBlur={handleBlur} className="h-[10%] w-full ">
-            <SimpleInput
-              holder="Verify Password"
-              type1="password"
-              type2="text"
-              icon={seePassword}
-              error={verror}
-              SetValue={setVerif}
-            />
-          </div>
-          <div className="h-[10%] w-full ">
-            <SimpleButton
-              buttonType="button"
-              content="Upload avatar"
-              icon={uploadIcon}
-              icon2={blackupload}
-            />
-          </div>
-          <div className="h-[10%] w-full ">
-            <SimpleButton buttonType="submit" content="Register" />
-          </div>
-          <div className="h-[10%] w-full flex flex-row justify-center space-x-2">
-            <div className="w-1/2 h-full">
-              <SimpleButton buttonType="button" icon={QuaranteDeux} icon2={blackQuarante} />
-            </div>
-            <div className="w-[50%] h-full">
-              <SimpleButton buttonType="button" icon={google} icon2={google} />
-            </div>
-          </div>
-        </form>
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex justify-center items-center bg-[#282828] w-screen h-screen">
+			<div className="px-2 py-1 flex flex-col justify-center min-w-[280px] min-h-[479px] w-full h-[90%] md:w-2/3  lg:w-1/3  lg:h-[80%] bg-black NeonShadowBord">
+				<div className="h-1/6 w-full  flex flex-col">
+					<div className="flex justify-end items-center h-1/5 w-full ">
+						<Link href="/">
+							<Image src={close} alt="close" className="w-10 h-10" />
+						</Link>
+					</div>
+					<div className="w-full h-4/6 text-white text-3xl font-bold NeonShadow font-Orbitron flex justify-center items-center">
+						<h1>Atari Pong</h1>
+					</div>
+				</div>
+				<form onSubmit={handleSubmit} className=" w-full h-5/6 flex  flex-col space-y-3 px-2">
+					<div className="h-[10%] w-full flex flex-row justify-center space-x-2">
+						<div  className="w-1/2 h-full">
+							<SimpleInput
+								holder="First Name"
+								type1="text"
+								SetValue={setFirstName}
+								setError={setFerror}
+								regex={Regex}
+								val = {Data.firstname}
+								isVerif = {false}
+
+
+							/>
+						</div>
+						<div  className="w-[50%] h-full">
+							<SimpleInput
+							 	holder="Last Name"
+							 	type1="text" 
+								SetValue={setLastName} 
+								setError={setLerror}
+								regex={Regex}
+								val = {Data.lastname}
+								isVerif = {false} 
+								
+								/>
+						</div>
+					</div>
+					<div  className="h-[10%] w-full ">
+						<SimpleInput 
+							holder="Username" 
+							type1="text" 
+							SetValue={setUsername} 
+							setError={setUerror}
+							regex={UserRegex}
+							val = {Data.username}
+							isVerif = {false}
+							/>
+					</div>
+					<div  className="h-[10%] w-full ">
+						<SimpleInput
+							holder="Mail@example.com"
+							type1="email"
+							SetValue={setEmail}
+							setError={setEerror}
+							regex={EmRegex}
+							val = {Data.email}
+							isVerif = {false}
+						/>
+					</div>
+					<div  className="h-[10%] w-full ">
+						<SimpleInput
+							holder="Password"
+							type1="password"
+							type2="text"
+							icon={seePassword}
+							SetValue={setPassword}
+							setError={setPerror}
+							regex={PassRegex}
+							val = {Data.password}
+							isVerif = {false}
+						/>
+					</div>
+					<div  className="h-[10%] w-full ">
+						<SimpleInput
+							holder="Verify Password"
+							type1="password"
+							type2="text"
+							icon={seePassword}
+							SetValue={setVerif}
+							setError={setVerror}
+							val = {Data.vpassword}
+							isVerif = {true}
+							pass = {Data.password}
+						/>
+					</div>
+					<div className="h-[10%] w-full ">
+						<SimpleButton
+							buttonType="button"
+							content="Upload avatar"
+							icon={uploadIcon}
+							icon2={blackupload}
+						/>
+					</div>
+					<div className="h-[10%] w-full ">
+						<SimpleButton buttonType="submit" content="Register" />
+					</div>
+					<div className="h-[10%] w-full flex flex-row justify-center space-x-2">
+						<div className="w-1/2 h-full">
+							<SimpleButton buttonType="button" icon={QuaranteDeux} icon2={blackQuarante} />
+						</div>
+						<div className="w-[50%] h-full">
+							<SimpleButton buttonType="button" icon={google} icon2={google} />
+						</div>
+					</div>
+				</form>
+			</div>
+		</main>
+	);
 }
 
 export default Register;
