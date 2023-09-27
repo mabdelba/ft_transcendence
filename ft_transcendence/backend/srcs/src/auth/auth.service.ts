4e4cf64ffcd47313742e129b92960d68f6146c60
aelabid <asmaa9920@hotmail.com>
2023-09-27 07:04:32 +0100

    modify 42 service to handel User already exists

--- ft_transcendence/backend/srcs/src/auth/auth.service.ts
+++ ft_transcendence/backend/srcs/src/auth/auth.service.ts
@@ -84,10 +84,17 @@ export class AuthService {
       throw new ForbiddenException('No user from 42');
     }
     try {
-      const user = await this.prisma.user.findUnique({
-        where: { login: req.login },
+      let user = await this.prisma.user.findUnique({
+        where: { login: req.login,  password: {not: null}},
+      });
+      if (user) {
+        throw new ForbiddenException('User already exists');
+      }
+      user = await this.prisma.user.findUnique({
+        where: { login: req.login},
       });
       if (!user) {
+        console.log("create new user");
         const newUser = await this.prisma.user.create({
           data: {
             login: req.login,
