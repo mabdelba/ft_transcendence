764b13bcc9359b0894a11e47f1eeb745e6d99820
mabdelba <abdelbar.mohamed1998@gmail.com>
2023-09-28 19:08:23 +0100

    add variables to Profil component: Uncompleted

--- ft_transcendence/frontend/srcs/app/components/forms/Profil.tsx
+++ ft_transcendence/frontend/srcs/app/components/forms/Profil.tsx
@@ -1,8 +1,10 @@
+'use client';
 import Avatar from '../../../public/avatar.svg';
 import Image  from 'next/image';
 import Percent from '../shapes/Percent';
 import online from '../../../public/online.svg';
 import offline from '../../../public/offline.svg';
+import { useState } from 'react';
 
 type profileProp = {
 
@@ -12,39 +14,46 @@ type profileProp = {
 	login: string;
 	online: boolean;
 	level: number;
+	percentage: number;
 	matchPlayed: number;
 	winPercent: number;             
 };
 
-function Profil() {
+function Profil(props: profileProp) {
 
 
-	const percentage = "w-[60%]";
+	const percentage = `w-[53%]`;
+	const win = `w-[70%]`;
+	const lose = `w-[30%]`;
+	
+	console.log(percentage, win, lose);
+
+	
 	return (
 
-		<div className="h-full w-full flex flex-col justify-center items-center bg-black NeonShadowBord">
+		<div className="h-full w-full flex flex-col  justify-center items-center bg-black NeonShadowBord">
 			<div className='h-1/2 w-full flex flex-row items-center '>
 				<div className='w-1/4 h-[50%] flex justify-end  '>
-					<Image src={Avatar} alt='avatar' className=''/>
+					<Image src={props.avatar} alt='avatar' className=''/>
 				</div>
 				<div className='w-3/4 h-[40%] flex flex-col justify-center items-start px-2'>
 					<div className='h-1/3 w-full -slate-700'>
-						Mohamed Abdelbar - mabdelba
+						{props.firstname} {props.lastname} - {props.login}
 					</div>
 					<div className='h-1/3 w-full -green-500 flex flex-row items-center pt-1.5'>
-						LvL 2 - 53%     -   	 
-						<Image src={online} alt="online" className='h-[95%]'/>
-						 Online
+						LvL {props.level} - {props.percentage}%		-	 
+						<Image src={props.online? online : offline} alt="online" className='h-[95%] ml-2'/>
+						 {props.online ? 'Online' : 'Offline'}
 					</div>
 					<div className='h-1/3 w-full -pink-500 pt-1.5'>
 						<Percent  width1={percentage} firstColor='bg-white' bord={true}/>
 					</div>
 				</div>
 			</div>
-			<div className='h-1/2 w-full flex flex-row text-xl'>
+			<div className='h-1/2 w-full flex flex-row text-xs md:text:base lg:text-xl'>
 				<div className='h-full w-1/2 flex flex-col items-center justify-center'>
 					<div className='h-1/2 w-full flex justify-center items-center'><h1>Matches Played</h1></div>
-					<div className='h-1/2 w-full flex justify-center items-start text-2xl'><h1>17</h1></div>
+					<div className='h-1/2 w-full flex justify-center items-start text-2xl'><h1>{props.matchPlayed}</h1></div>
 				</div>
 				<div className='h-full w-1/2 flex flex-col items-center'>
 					<div className='h-1/2 w-full flex justify-center items-center -green-500'>
@@ -52,10 +61,10 @@ function Profil() {
 					</div>
 					<div className='h-1/2 w-[80%] flex flex-col justify-start items-center -blue-600'>
 						<div className='w-[90%] flex flex-row justify-between items-start text-sm'>
-							<div>72%</div>
-							<div>28%</div>
+							<div>{props.winPercent}%</div>
+							<div>{100 - props.winPercent}%</div>
 						</div>
-						<Percent bord={false} width1={percentage} firstColor='bg-[#00B2FF]' width2='w-[40%]' secondColor='bg-[#FF0742]'/>
+						<Percent bord={false} width1={win} firstColor='bg-[#00B2FF]' width2={lose} secondColor='bg-[#FF0742]'/>
 					</div>					
 				</div>
 			</div>
--- ft_transcendence/frontend/srcs/app/dashboard/page.tsx
+++ ft_transcendence/frontend/srcs/app/dashboard/page.tsx
@@ -1,12 +1,21 @@
 import Profil from '../components/forms/Profil';
+import Avatar from '../../public/avatar.svg';
 
 function Dashboard() {
   return (
     <main className='w-screen h-screen flex justify-center items-center font-Orbitron NeonShadow'>
       <div className='w-1/3 h-1/3'>
-        <Profil />
+        <Profil  avatar={Avatar}
+          firstname='Mohamed'
+          lastname='Boundagani' 
+          login='ahel-bah' 
+          online={true} 
+          matchPlayed={0}
+          winPercent={70}
+          level={2}
+          percentage={53}
+        />
       </div>
-      
     </main>
   );
 }
