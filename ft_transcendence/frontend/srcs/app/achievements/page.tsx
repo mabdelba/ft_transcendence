7212062cb84edf6e5572627711aeef21df179651
mabdelba <abdelbar.mohamed1998@gmail.com>
2023-10-03 01:47:24 +0100

    achievement page responsive : need to link with backend

---
+++ ft_transcendence/frontend/srcs/app/achievements/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+import DiscloComp from "../components/shapes/DiscloComp";
+
+
+
+function Achievements(){
+
+	const  Acquired = [ 'Facebook', 'Whatsapp', 'Instagram', 'Discord'];
+	const  Unacquired = ['achiev 1', 'achiev 2', 'Telegram', 'Tik Tok', 'YouTube', 'Snapchat', 'X', 'Netflix'];
+	return (
+
+		<main className="w-screen h-auto md:h-screen flex flex-col font-Orbitron min-h-[480px] min-w-[280px]">
+			<div className='w-full h-12 md:h-[10%] pl-6 md:pl-12 NeonShadow flex justify-start items-center text-base xl:text-3xl -yellow-300'>
+			 	All achievements
+			</div>
+			<div className="w-full h-auto flex flex-col px-0  md:px-12 space-y-8 md:space-y-12">
+				<div className="w-full  h-auto ">
+					<DiscloComp
+						title="Acquired achievements"
+						divArray={Acquired}
+						textColor="text-white NeonShadow"
+						Color={true}
+						hoverColor="hover:text-[#00B2FF] hover:blueShadow"
+					/>
+				</div>
+				<div className="w-full h-auto ">
+					<DiscloComp 
+						title="Unacquired achievements"
+						divArray={Unacquired}
+						textColor="white NeonShadow"
+						Color={false}
+						hoverColor="hover:text-[#FF0742] hover:redShadow"
+					/>
+				</div>
+			</div>
+		</main>
+	);
+
+
+}
+
+export default Achievements;
--- ft_transcendence/frontend/srcs/app/dashboard/page.tsx
+++ ft_transcendence/frontend/srcs/app/dashboard/page.tsx
@@ -35,7 +35,7 @@ function Dashboard() {
   
   return (
     <main className='h-auto w-auto md:w-screen md:h-screen font-Orbitron NeonShadow min-h-[480px] min-w-[280px]'>
-      <div className='w-full h-[8%] pl-6 md:pl-12 font-semibold flex justify-start items-center text-base xl:text-3xl'>
+      <div className='w-full h-[8%] pl-6 md:pl-12 font-semibold flex justify-start items-center NeonShadow text-base xl:text-3xl'>
         Hello {firstName}!
       </div>
       <div className=' w-full md:h-[84%] h-auto flex flex-col md:flex-row justify-center items-center px-2 md:px-12 space-y-6 md:space-y-0 md:space-x-6 xl:space-x-12 '>
