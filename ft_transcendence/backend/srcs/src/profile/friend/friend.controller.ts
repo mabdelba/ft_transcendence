cc6a61f024770d1fe59d22afde108391d1724fab
aelabid <asmaa9920@hotmail.com>
2023-10-06 14:01:18 +0100

    work on friend backend logic
    add friend request
    accept friend request
    decline friend request
    remove friend
    block user
    unblock user

--- ft_transcendence/backend/srcs/src/profile/friend/friend.controller.ts
+++ ft_transcendence/backend/srcs/src/profile/friend/friend.controller.ts
@@ -12,9 +12,31 @@ export class FriendController {
   addFriend(@Req() req: Request, @Body() dto: { recieverId: number }) {
     return this.friendService.sendFriendRequest(req.user as User, dto.recieverId);
   }
+
   @Post('accept-friend-request')
   @UseGuards(JwtGuard)
   acceptFriendRequest(@Req() req: Request, @Body() dto: { senderId: number }) {
     return this.friendService.acceptFriendRequest(req.user as User, dto.senderId);
+  }
+
+  @Post('reject-friend-request')
+  @UseGuards(JwtGuard)
+  rejectFriendRequest(@Req() req: Request, @Body() dto: { senderId: number }) {
+    return this.friendService.rejectFriendRequest(req.user as User, dto.senderId);
+  }
+  @Post('remove-friend')
+  @UseGuards(JwtGuard)
+  removeFriend(@Req() req: Request, @Body() dto: { friendId: number }) {
+    return this.friendService.removeFriend(req.user as User, dto.friendId);
+  }
+  @Post('block-user')
+  @UseGuards(JwtGuard)
+  blockUser(@Req() req: Request, @Body() dto: { userId: number }) {
+    return this.friendService.blockUser(req.user as User, dto.userId);
+  }
+  @Post('unblock-user')
+  @UseGuards(JwtGuard)
+  unblockUser(@Req() req: Request, @Body() dto: { userId: number }) {
+    return this.friendService.unblockUser(req.user as User, dto.userId);
   }
 }
