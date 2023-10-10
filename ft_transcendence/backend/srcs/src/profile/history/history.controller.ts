966aa8283cdce0c19686aa6efff5817f9bdf0b40
aelabid <asmaa9920@hotmail.com>
2023-10-10 22:54:02 +0100

    work on history backend

---
+++ ft_transcendence/backend/srcs/src/profile/history/history.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { HistoryService } from './history.service';
+import { JwtGuard } from 'src/auth/guards';
+
+@Controller('history')
+export class HistoryController {
+    constructor(private historyService: HistoryService) {}
+    @Get()
+    @UseGuards(JwtGuard)
+    getAllMatchesPlayed(@Req() req) {
+        return this.historyService.getAllMatchesPlayed(req.user.login);
+    }
+}
