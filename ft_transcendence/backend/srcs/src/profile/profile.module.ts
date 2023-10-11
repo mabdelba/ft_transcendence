35758ddb805b23b3b2b91fba551be0c2f213abe9
aelabid <asmaa9920@hotmail.com>
2023-10-11 02:38:53 +0100

    work on settings backend

--- ft_transcendence/backend/srcs/src/profile/profile.module.ts
+++ ft_transcendence/backend/srcs/src/profile/profile.module.ts
@@ -3,10 +3,11 @@ import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 import { UserModule } from './user/user.module';
 import { FriendModule } from './friend/friend.module';
+import { SettingsModule } from './settings/settings.module';
 
 @Module({
   controllers: [ProfileController],
   providers: [ProfileService],
-  imports: [UserModule, FriendModule],
+  imports: [UserModule, FriendModule, SettingsModule],
 })
 export class ProfileModule {}
