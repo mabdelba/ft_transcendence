59a72ec8bfc9a07e298ae51b7e46133c51995871
Ahmed El bahri <ahel-bah@e3r7p14.1337.ma>
2023-11-07 03:10:53 +0100

    players status bar

--- ft_transcendence/frontend/srcs/app/game/page.tsx
+++ ft_transcendence/frontend/srcs/app/game/page.tsx
@@ -4,8 +4,24 @@ import Game from './game';
 import Matter from 'matter-js';
 import { useState, useEffect , useRef} from 'react';
 import { Socket, io } from 'socket.io-client';
+import { Content } from 'next/font/google';
+import Alien from '../../public/alien.svg';
+import Logout from '../../public/log-out.svg';
+import Image from 'next/image';
+import { set } from 'husky';
+import { Console } from 'console';
 
 let game: Game | null = null;
+
+const fetchInfo = async () => {
+    const apiUrl = 'http://localhost:3000/api/atari-pong/v1/user/me-from-token';
+    const token = localStorage.getItem('jwtToken');
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const res = await fetch(apiUrl, config);
+    return res.json();
+  };
 
 const gamePage = () => {
     const gameDiv = useRef<HTMLDivElement>(null)
@@ -48,22 +64,57 @@ const gamePage = () => {
         });
 
         setGameSocket(socket);
-        
+
         return () => {
             window.removeEventListener('resize', handleResize)
         }
     }, [])
 
+    const [username, setUsername] = useState("Username");
+    const [avatar, setAvatar] = useState(Alien);
+
+    fetchInfo()
+        .then((data) => {
+            setUsername(data.login);
+            // setAvatar(data.avatar);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+        console.log(avatar);
+
 
     return (
-        <div className='w-screen h-screen flex flex-col justify-center items-center'>
-            <button
+        <div className='font-Orbitron w-screen h-screen flex flex-col justify-center items-center'>
+            <div className='flex flex-row items-center w-[100%] justify-evenly'>
+                <div className="flex flex-row items-center m-2">
+                    <div className='NeonShadowBord h-[70px] w-[70px] md:w-[90px] md:h-[90px] m-auto flex'>
+                        <Image src={avatar} alt='avatar' className="" />
+                    </div>
+                    <div className="blueShadow text-[20px] text-[#00B2FF] m-2 hidden md:block">{username}</div>
+                </div>
+                <button type="button" className="NeonShadowBord flex flex-row items-center h-fit px-4 py-3 hover:bg-white hover:text-[black] transition-[300]">
+                    <Image src={Logout} className='h-[20px]' />
+                    <div className="hidden md:block pl-2 text-[15px]">Leave</div>
+                </button>
+                <div className="flex flex-row items-center m-2">
+                    <div className="redShadow text-[20px] text-[#FF0742] m-2 hidden md:block">{username}</div>
+                    <div className='NeonShadowBord h-[70px] w-[70px] md:w-[90px] md:h-[90px] m-auto flex'>
+                        <Image src={avatar} alt='avatar' className="" />
+                    </div>
+                </div>
+            </div>
+            <div className='text-[40px]'>
+                0 - 0
+            </div>
+            {/* <button
                 type='button'
                 onClick={()=> {
                     // console.log(gameSocket?.id);
                     gameSocket?.emit('StartGame', {map: 'map1'})}}
-                 className='z-10 h-10 w-52 border'>Start Game</button>
-            <div className='w-[80%] h-[90%] bg-red-500 flex justify-center items-center' ref={gameDiv}>               
+                 className='z-10 h-10 w-52 border'>Start Game</button> */}
+            <div className='w-[100%] h-[80%] flex justify-center items-center' ref={gameDiv}>
             </div>
 
         </div>
